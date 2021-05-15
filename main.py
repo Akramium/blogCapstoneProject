89d64bf88d0781b92b405bbdc4e499f93a978459
akramuim <o.akramuim@gmail.com>
2021-05-15 05:19:01 +0000

    blog

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+posts = requests.get(url="https://api.npoint.io/0067e63917ca7a5034d9").json()
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    requested_post = None
+    for blog_post in posts:
+        if post_id == blog_post["id"]:
+            requested_post = blog_post
+    return render_template('post.html', post=requested_post)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
